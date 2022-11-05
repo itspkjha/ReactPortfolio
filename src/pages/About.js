ed54fd9b54e93f42ea5412254be42f1892a067e4
itspkjha <86141517+itspkjha@users.noreply.github.com>
2022-11-05 21:53:04 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,7 +11,12 @@ const About = () => {
                     <div className='w-full md:h-full h-[90%] overflow-y-auto md:flex'>
                         <div className="md:flex md:flex-col md:h-full md:w-[60%] md:items-start md:justify-center">
                             <h1 className=" w-full text-start md:text-6xl text-xl font-bold">About Me</h1>
-                            <p className="md:text-2xl w-full mt-5 md:text-start text-justify"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <p className="md:text-2xl w-full mt-5 md:text-start text-justify"><span>Hi, I am Paritosh Kumar Jha !</span>
+                                <br></br>
+                                <span>I am passionate about front-end development.</span>
+                                <br></br>
+                                <span>Currently in my third year of Btech in Information Technology. I enjoy development and am constantly looking for an environment where I'll be able to learn, connect and grow. </span>
+                                <br></br></p>
                         </div>
                         <div className="h-full md:flex hidden  justify-center items-center w-[30%]">
                             <img src={require('../logo.png')} alt="logo-img" className=" w-full aspect-square rounded-full" />
